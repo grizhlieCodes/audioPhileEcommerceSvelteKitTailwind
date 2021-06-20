baca27b36432fceeebaf3d9d8598928adfe81bc4
grizhlieCodes <rafal.potasz@gmail.com>
2021-06-20 01:35:32 +0100

    Added homepage button

--- src/lib/UI/Button.svelte
+++ src/lib/UI/Button.svelte
@@ -8,15 +8,14 @@
 
 {#if btnType === 'primary'}
 	<button
-		class="text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem] 
+		class="text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
 		hover:bg-lightOrange text-white tracking-[1px] transition-all">
 		{content}
 	</button>
 {:else if btnType === 'secondary'}
 	<button
-		class="text-[1.3rem] font-bold uppercase bg-white px-[3.4rem] py-[1.5rem] 
-		hover:bg-black hover:text-white text-black tracking-[1px] border-[1px] border-black
-		transition-all">
+		class="text-[1.3rem] font-bold uppercase bg-white px-[3.4rem] py-[1.5rem] hover:bg-black
+		hover:text-white text-black tracking-[1px] border-[1px] border-black transition-all">
 		{content}
 	</button>
 {:else if btnType === 'minimal'}
@@ -27,5 +26,11 @@
 		<div class="transform group-hover:transform group-hover:rotate-180 transition-all">
 			<img src="./static/images/shared/desktop/icon-arrow-right.svg" alt="" />
 		</div>
+	</button>
+{:else if btnType === 'homepage'}
+	<button
+		class="text-[1.3rem] font-bold uppercase bg-black px-[3.4rem] py-[1.5rem] hover:bg-[#4C4C4C]
+		hover:text-white text-white tracking-[1px] transition-all">
+		{content}
 	</button>
 {/if}
