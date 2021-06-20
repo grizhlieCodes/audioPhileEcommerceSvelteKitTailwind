ffc80376e26fade08cbd7a5473e6ecb894b44aef
grizhlieCodes <rafal.potasz@gmail.com>
2021-06-20 16:40:56 +0100

    Cleanup and added Header component to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,7 @@
 <script>
-
+import Header from '$lib/navigation/Header.svelte'
 import {writable} from 'svelte/store'
-import { setContext, onMount } from 'svelte'
+import { setContext } from 'svelte'
 
     let width
     let size = writable('')
@@ -16,25 +16,26 @@ import { setContext, onMount } from 'svelte'
     }
 
 </script>
+<svelte:window bind:innerWidth={width}/>
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-
-
     :global(:root) {
         font-family: 'manrope', sans-serif;
         font-size: 62.5%;
     }
+    :global(*){
+        box-sizing: border-box;
+    }
 </style>
 
-<svelte:window bind:innerWidth={width}/>
+<Header />
+<main>
 
-<!-- HEADER -->
-
-
+</main>
 <!-- MAIN -->
 <slot />
 
