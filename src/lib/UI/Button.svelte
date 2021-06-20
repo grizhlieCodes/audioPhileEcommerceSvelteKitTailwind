5ff5d7e7cf772c79fc3f9870eac6e2105dd42d3d
grizhlieCodes <rafal.potasz@gmail.com>
2021-06-20 16:41:05 +0100

    Updated button component

--- src/lib/UI/Button.svelte
+++ src/lib/UI/Button.svelte
@@ -1,5 +1,5 @@
 <script>
-	export let content, btnType;
+	export let content, btnType, link = '';
 </script>
 
 <style>
@@ -7,30 +7,39 @@
 </style>
 
 {#if btnType === 'primary'}
+
 	<button
 		class="text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
 		hover:bg-lightOrange text-white tracking-[1px] transition-all">
 		{content}
 	</button>
+	
 {:else if btnType === 'secondary'}
+
 	<button
 		class="text-[1.3rem] font-bold uppercase bg-white px-[3.4rem] py-[1.5rem] hover:bg-black
 		hover:text-white text-black tracking-[1px] border-[1px] border-black transition-all">
 		{content}
 	</button>
-{:else if btnType === 'minimal'}
-	<button
+	
+{:else if btnType === 'link'}
+
+	<a 
+		href="{link}"
 		class="text-[1.3rem] font-bold uppercase hover:text-darkOrange text-black tracking-[1px]
-		transition-all flex flex-row justify-center items-center h-[48px] group">
+		transition-all flex flex-row justify-center items-center group">
 		<p class="mr-[1.3rem]">{content}</p>
 		<div class="transform group-hover:transform group-hover:rotate-180 transition-all">
 			<img src="./static/images/shared/desktop/icon-arrow-right.svg" alt="" />
 		</div>
-	</button>
+	</a>
+	
 {:else if btnType === 'homepage'}
+
 	<button
 		class="text-[1.3rem] font-bold uppercase bg-black px-[3.4rem] py-[1.5rem] hover:bg-[#4C4C4C]
 		hover:text-white text-white tracking-[1px] transition-all">
 		{content}
 	</button>
+	
 {/if}
