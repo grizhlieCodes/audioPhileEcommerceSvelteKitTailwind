a0a776dbf06d2c620190c5edacaa5202c6b3e7cf
grizhlieCodes <rafal.potasz@gmail.com>
2021-06-28 14:39:10 +0100

    File styling changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,44 +1,43 @@
 <script>
-import Header from '$lib/navigation/Header.svelte'
-import {writable} from 'svelte/store'
-import { setContext } from 'svelte'
+	import Header from '$lib/navigation/Header.svelte';
+	import { writable } from 'svelte/store';
+	import { setContext } from 'svelte';
 
-    let width
-    let size = writable('')
-    setContext('size', size)
+	let width;
+	let size = writable('');
+	setContext('size', size);
 
-    $: if(width >= 1300){
-        size.set('desktop') 
-    } else if (width >= 768 ){
-        size.set('tablet')
-    } else {
-        size.set('mobile')
-    }
-
+	$: if (width >= 1024) {
+		size.set('desktop');
+	} else if (width >= 768) {
+		size.set('tablet');
+	} else {
+		size.set('mobile');
+	}
 </script>
-<svelte:window bind:innerWidth={width}/>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+	
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-    :global(:root) {
-        font-family: 'manrope', sans-serif;
-        font-size: 62.5%;
-    }
-    :global(*){
-        box-sizing: border-box;
-    }
+
+	:global(:root) {
+		font-family: 'manrope', sans-serif;
+		font-size: 62.5%;
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+	}
+
 </style>
 
+<svelte:window bind:innerWidth={width} />
 <Header />
-<main>
-
-</main>
+<main />
 <!-- MAIN -->
 <slot />
 
-
 <!-- FOOTER -->
-
