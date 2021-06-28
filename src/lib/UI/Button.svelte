02c51e95de9ffd7c1f4d0475badf562ea9a3515a
grizhlieCodes <rafal.potasz@gmail.com>
2021-06-29 00:18:57 +0100

    Updated buttons for links

--- src/lib/UI/Button.svelte
+++ src/lib/UI/Button.svelte
@@ -1,48 +1,57 @@
 <script>
-	export let content, btnType, link = '';
+	export let content,
+		btnType,
+		link = '';
 </script>
 
 <style>
-	button, a{
+	button,
+	a {
 		grid-area: button;
 	}
 </style>
 
 {#if btnType === 'primary'}
 
-	<a 
-		href="{link}"
-		class="text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
-		hover:bg-lightOrange text-white tracking-[1px] transition-all w-max">
-		{content}
-	</a>
-	
+	{#if link === ''}
+		<button
+			class="text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
+			hover:bg-lightOrange text-white tracking-[1px] transition-all w-max" on:click>
+			{content}
+		</button>
+	{:else}
+		<a
+			href={link}
+			class="text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
+			hover:bg-lightOrange text-white tracking-[1px] transition-all w-max">
+			{content}
+		</a>
+	{/if}
 {:else if btnType === 'secondary'}
 
-	<a href="{link}"
+	<a
+		href={link}
 		class="text-[1.3rem] font-bold uppercase bg-white px-[3.4rem] py-[1.5rem] hover:bg-black
 		hover:text-white text-black tracking-[1px] border-[1px] border-black transition-all w-max">
 		{content}
 	</a>
-	
 {:else if btnType === 'link'}
 
-	<a 
-		href="{link}"
+	<a
+		href={link}
 		class="text-[1.3rem] font-bold uppercase hover:text-darkOrange text-black tracking-[1px]
-		transition-all flex flex-row justify-center items-center group-hover:text-darkOrange  w-max">
+		transition-all flex flex-row justify-center items-center group-hover:text-darkOrange w-max">
 		<p class="mr-[1.3rem]">{content}</p>
 		<div class="transform group-hover:transform group-hover:rotate-180 transition-all">
 			<img src="/images/shared/desktop/icon-arrow-right.svg" alt="" />
 		</div>
 	</a>
-	
 {:else if btnType === 'homepage'}
 
-	<a href={link}
-		class="text-[1.3rem] font-bold uppercase bg-black px-[3.4rem] py-[1.5rem] w-max hover:bg-[#4C4C4C]
-		hover:text-white text-white tracking-[1px] transition-all">
+	<a
+		href={link}
+		class="text-[1.3rem] font-bold uppercase bg-black px-[3.4rem] py-[1.5rem] w-max
+		hover:bg-[#4C4C4C] hover:text-white text-white tracking-[1px] transition-all">
 		{content}
 	</a>
-	
 {/if}
