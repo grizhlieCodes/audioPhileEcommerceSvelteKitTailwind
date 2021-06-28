866fc797f17555a001ebe89c6afdeb0edf6117b1
grizhlieCodes <rafal.potasz@gmail.com>
2021-06-28 14:34:54 +0100

    Configured tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
+  darkMode: 'media',
   purge: ['./src/**/*.svelte'],
   theme: {
+    screens: {
+      mTab: '400px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     fontFamily: {
       'sans': ['manrope']
     },
@@ -13,9 +24,13 @@ module.exports = {
         nearWhite: '#FAFAFA',
         lightOrange: '#fbaf85'
       },
-      screens: {
-        'mTab': '400px'
-      }
+      // screens: {
+      //   mTab: '400px',
+      //   ...defaultTheme.screens,
+      // },
+      backgroundImage: theme => ({
+        'home-zx9-mob': "url('./static/images/')"
+      })
     }
   }
 }
