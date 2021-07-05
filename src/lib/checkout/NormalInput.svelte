e0c0b06dcea23ca1732d5756917c10cbdff645bd
grizhlieCodes <rafal.potasz@gmail.com>
2021-07-05 15:26:17 +0100

    Improved validity logic for input + added blur/touch

--- src/lib/checkout/NormalInput.svelte
+++ src/lib/checkout/NormalInput.svelte
@@ -4,7 +4,8 @@
 		type = 'text',
 		width = '',
 		value = '',
-		isValid = true;
+		isValid = true,
+		touched = false;
 
 	// import {createEventDispatcher} from 'svelte'
 	// const dispatch = createEventDispatcher();
@@ -33,7 +34,7 @@
 	<div class="input-field min-w-[28rem] flex-auto {width === 'full' ? 'w-full' : ''}">
 		<div class="container flex flex-row justify-between w-full items-center mb-[0.9rem] ">
 			<p class=" text-[1.2rem] font-bold tracking-[-0.21px] ">{name}</p>
-			{#if !isValid}
+			{#if !isValid && touched }
 				<p class=" text-red-500 text-[1.2rem] font-bold tracking-[-0.21px] ">
 					{#if type === 'email'}
 						Wrong Format
@@ -44,12 +45,13 @@
 			{/if}
 		</div>
 		<input
+			on:blur={() => touched = true}
 			{type}
 			{placeholder}
 			{value}
 			on:input
 			class=" w-full h-[5.6rem] rounded-[0.8rem] focus-within:border-[2px] outline-none px-[2.4rem]
-			font-bold text-[1.4rem] tracking-[-0.25px] transition-all {isValid ? 'border-[1px] border-[#CFCFCF] text-black focus-within:border-darkOrange' : 'border-red-500 border-[2px] text-red-500'}
+			font-bold text-[1.4rem] tracking-[-0.25px] transition-all {!isValid && touched ? 'border-red-500 border-[2px] text-red-500' : 'border-[1px] border-[#CFCFCF] text-black focus-within:border-darkOrange'  }
 			" />
 	</div>
 {:else}
@@ -57,7 +59,7 @@
 	<div class="input-field min-w-[28rem] flex-auto lg:max-w-[33.3rem]">
 		<div class="container flex flex-row justify-between w-full items-center mb-[0.9rem] ">
 			<p class=" text-[1.2rem] font-bold tracking-[-0.21px] ">{name}</p>
-			{#if !isValid}
+			{#if !isValid && touched }
 				<p class=" text-red-500 text-[1.2rem] font-bold tracking-[-0.21px] ">
 					{#if type === 'email'}
 							Wrong Format
@@ -68,11 +70,12 @@
 			{/if}
 		</div>
 		<input
+			on:blur={() => touched = true}
 			{type}
 			{placeholder}
 			{value}
 			class=" w-full h-[5.6rem] rounded-[0.8rem] focus-within:border-[2px] outline-none px-[2.4rem]
-			font-bold text-[1.4rem] tracking-[-0.25px] caret-red-500 {isValid ? 'border-[1px] border-[#CFCFCF] text-black focus-within:border-darkOrange' : 'border-red-500 border-[2px] text-red-500'}
+			font-bold text-[1.4rem] tracking-[-0.25px] caret-red-500 {!isValid && touched ? 'border-red-500 border-[2px] text-red-500'  : 'border-[1px] border-[#CFCFCF] text-black focus-within:border-darkOrange' }
 			"
 			on:input />
 	</div>
