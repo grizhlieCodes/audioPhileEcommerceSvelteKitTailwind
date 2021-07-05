cbbb8720b2f73510dda5c4a32581c10259c6afce
grizhlieCodes <rafal.potasz@gmail.com>
2021-07-05 14:55:54 +0100

    Added validation styling to inputs

--- src/lib/checkout/NormalInput.svelte
+++ src/lib/checkout/NormalInput.svelte
@@ -3,7 +3,8 @@
 		placeholder = '',
 		type = 'text',
 		width = '',
-		value = '';
+		value = '',
+		isValid = true;
 
 	// import {createEventDispatcher} from 'svelte'
 	// const dispatch = createEventDispatcher();
@@ -30,27 +31,49 @@
 {#if width === 'full'}
 	<!-- content here -->
 	<div class="input-field min-w-[28rem] flex-auto {width === 'full' ? 'w-full' : ''}">
-		<p class=" text-[1.2rem] font-bold tracking-[-0.21px] mb-[0.9rem]">{name}</p>
+		<div class="container flex flex-row justify-between w-full items-center mb-[0.9rem] ">
+			<p class=" text-[1.2rem] font-bold tracking-[-0.21px] ">{name}</p>
+			{#if !isValid}
+				<p class=" text-red-500 text-[1.2rem] font-bold tracking-[-0.21px] ">
+					{#if type === 'email'}
+						Wrong Format
+					{:else}
+						Can't be empty
+					{/if}
+				</p>
+			{/if}
+		</div>
 		<input
 			{type}
 			{placeholder}
 			{value}
-			class=" w-full h-[5.6rem] rounded-[0.8rem] border-[1px] border-[#CFCFCF]
-			focus-within:border-darkOrange focus-within:border-[2px] outline-none px-[2.4rem] font-bold
-			text-black text-[1.4rem] tracking-[-0.25px] caret-red-500 transition-all"
-			on:input />
+			on:input
+			class=" w-full h-[5.6rem] rounded-[0.8rem] focus-within:border-[2px] outline-none px-[2.4rem]
+			font-bold text-[1.4rem] tracking-[-0.25px] transition-all {isValid ? 'border-[1px] border-[#CFCFCF] text-black focus-within:border-darkOrange' : 'border-red-500 border-[2px] text-red-500'}
+			" />
 	</div>
 {:else}
 	<!-- else content here -->
 	<div class="input-field min-w-[28rem] flex-auto lg:max-w-[33.3rem]">
-		<p class=" text-[1.2rem] font-bold tracking-[-0.21px] mb-[0.9rem]">{name}</p>
+		<div class="container flex flex-row justify-between w-full items-center mb-[0.9rem] ">
+			<p class=" text-[1.2rem] font-bold tracking-[-0.21px] ">{name}</p>
+			{#if !isValid}
+				<p class=" text-red-500 text-[1.2rem] font-bold tracking-[-0.21px] ">
+					{#if type === 'email'}
+							Wrong Format
+					{:else}
+						Can't be empty
+					{/if}
+				</p>
+			{/if}
+		</div>
 		<input
 			{type}
 			{placeholder}
 			{value}
-			class=" w-full h-[5.6rem] rounded-[0.8rem] border-[1px] border-[#CFCFCF]
-			focus-within:border-darkOrange focus-within:border-[2px] outline-none px-[2.4rem] font-bold
-			text-black text-[1.4rem] tracking-[-0.25px] caret-red-500"
+			class=" w-full h-[5.6rem] rounded-[0.8rem] focus-within:border-[2px] outline-none px-[2.4rem]
+			font-bold text-[1.4rem] tracking-[-0.25px] caret-red-500 {isValid ? 'border-[1px] border-[#CFCFCF] text-black focus-within:border-darkOrange' : 'border-red-500 border-[2px] text-red-500'}
+			"
 			on:input />
 	</div>
 {/if}
