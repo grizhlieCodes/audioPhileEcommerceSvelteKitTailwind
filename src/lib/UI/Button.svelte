108a080b21cd76642e1e6ab35cecd8f927b45d95
grizhlieCodes <rafal.potasz@gmail.com>
2021-07-05 14:57:18 +0100

    Styling adjustments to buttons

--- src/lib/UI/Button.svelte
+++ src/lib/UI/Button.svelte
@@ -2,7 +2,8 @@
 	export let content,
 		btnType,
 		type = 'button',
-		link = '';
+		link = '',
+		isDisabled = false;
 </script>
 
 <style>
@@ -27,7 +28,7 @@
 			on:click
 			sveltekit:prefetch
 			href={link}
-			class="text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
+			class=" text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
 			hover:bg-lightOrange text-white tracking-[1px] transition-all w-max block
 			group-hover:bg-lightOrange">
 			{content}
@@ -38,16 +39,16 @@
 	<a
 		sveltekit:prefetch
 		href={link}
-		class="text-[1.3rem] font-bold uppercase bg-white px-[3.4rem] py-[1.5rem] hover:bg-black
+		class="block text-[1.3rem] font-bold uppercase bg-white px-[3.4rem] py-[1.5rem] hover:bg-black
 		hover:text-white text-black tracking-[1px] border-[1px] border-black transition-all w-max">
 		{content}
 	</a>
 {:else if btnType === 'link'}
 
-	<a
+	<a  disabled={isDisabled ? 'true' : false}
 		sveltekit:prefetch
 		href={link}
-		class="text-[1.3rem] font-bold uppercase hover:text-darkOrange text-black tracking-[1px]
+		class=" text-[1.3rem] font-bold uppercase hover:text-darkOrange text-black tracking-[1px]
 		transition-all flex flex-row justify-center items-center group-hover:text-darkOrange w-max">
 		<p class="mr-[1.3rem]">{content}</p>
 		<div class="transform group-hover:transform group-hover:rotate-180 transition-all">
@@ -59,7 +60,7 @@
 	<a
 		sveltekit:prefetch
 		href={link}
-		class="text-[1.3rem] font-bold uppercase bg-black px-[3.4rem] py-[1.5rem] w-max
+		class=" text-[1.3rem] font-bold uppercase bg-black px-[3.4rem] py-[1.5rem] w-max
 		hover:bg-[#4C4C4C] hover:text-white text-white tracking-[1px] transition-all">
 		{content}
 	</a>
@@ -67,8 +68,8 @@
 
 	{#if link !== ''}
 		<a
-			href={link}
-			class="text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
+			href={isDisabled ? '' : link}
+			class="block text-[1.3rem] font-bold uppercase bg-darkOrange px-[3.4rem] py-[1.5rem]
 			hover:bg-lightOrange text-white tracking-[1px] transition-all w-full text-center"
 			on:click
 			type>
