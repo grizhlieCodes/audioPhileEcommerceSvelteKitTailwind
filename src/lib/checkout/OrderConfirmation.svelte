fc10d94beb3d9f95506c9852f54222213fddde79
grizhlieCodes <rafal.potasz@gmail.com>
2021-07-06 21:59:13 +0100

    Fixed gap between products

--- src/lib/checkout/OrderConfirmation.svelte
+++ src/lib/checkout/OrderConfirmation.svelte
@@ -2,9 +2,9 @@
 	import { slide } from 'svelte/transition';
 	import userStore from './userStore.js';
 	import productsStore from '$lib/products/cartStore.js';
-    import Button from '$lib/UI/Button.svelte'
-    import {createEventDispatcher, onMount} from 'svelte'
-    const dispatch = createEventDispatcher();
+	import Button from '$lib/UI/Button.svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
+	const dispatch = createEventDispatcher();
 
 	let grandTotal = 50;
 
@@ -16,13 +16,11 @@
 	});
 
 	onMount(() => {
-
-		
-		const orderConfirmation = document.querySelector('.order-confirmation')
-					const oConfPosition = orderConfirmation.getBoundingClientRect().top
-					const options = { top: oConfPosition - 50, left: 0, behavior: 'smooth' };
-					window.scrollTo(options);
-	})
+		setTimeout(() => {
+			const options = { top: 0, left: 0, behavior: 'smooth' };
+			window.scrollTo(options);
+		},1000)
+	});
 </script>
 
 <style>
@@ -34,8 +32,10 @@
 <div
 	class="order-confirmation w-full h-full bg-white p-[2.4rem] rounded-[8px] flex flex-col
 	items-start gap-y-[3.1rem] max-w-[60rem] sm:px-[11.5rem]">
-	<h2 class=" text-[1.8rem] font-bold tracking-[1.29px] text-black uppercase w-full text-center ">confirm order</h2>
-	<div id="items-list" class="top w-full min-h-52 flex flex-col flex-1 ">
+	<h2 class=" text-[1.8rem] font-bold tracking-[1.29px] text-black uppercase w-full text-center ">
+		confirm order
+	</h2>
+	<div id="items-list" class="top w-full min-h-52 flex flex-col flex-1 gap-y-[2.4rem] ">
 		{#each $productsStore as product, i}
 			<div class=" flex items-center gap-x-[1.6rem]" transition:slide={{ duration: 150 }}>
 				<img
@@ -60,17 +60,30 @@
 		<h3 class=" text-[1.5rem] text-black opacity-50 uppercase font-light flex-1 ">grand total</h3>
 		<h3 class=" text-[1.8rem] text-darkOrange font-bold">$ {grandTotal.toLocaleString()}</h3>
 	</div>
-    <div class="text-container">
-        <h2 class=" text-[1.5rem] md:text-[1.8rem] leading-[2.5rem] text-black opacity-50 font-bold mb-[0.8rem] ">
-            To be delivered to {$userStore.name} 
-        </h2>
-        <p class=" text-[1.5rem] leading-[2.5rem] text-black opacity-50 text-left "> {$userStore.address} </p>
-        <p class=" text-[1.5rem] leading-[2.5rem] text-black opacity-50 text-left "> {$userStore.city} </p>
-        <p class=" text-[1.5rem] leading-[2.5rem] text-black opacity-50 text-left "> {$userStore.zip} </p>
-        <p class=" text-[1.5rem] leading-[2.5rem] text-black opacity-50 text-left "> {$userStore.country} </p>
-    </div>
-    <div class="buttons flex flex-col gap-y-[1.5rem] justify-between items-center w-full sm:flex-row ">
-        <Button btnType="grey" content="save for later" on:click={() => dispatch('saveForLater')}  link="/" />
-        <Button btnType="primary" content="place order" on:click={() => dispatch('placeOrder')} />
-    </div>
+	<div class="text-container">
+		<h2
+			class=" text-[1.5rem] md:text-[1.8rem] leading-[2.5rem] text-black opacity-50 font-bold
+			mb-[0.8rem] ">
+			To be delivered to {$userStore.name}
+		</h2>
+		<p class=" text-[1.5rem] leading-[2.5rem] text-black opacity-50 text-left ">
+			{$userStore.address}
+		</p>
+		<p class=" text-[1.5rem] leading-[2.5rem] text-black opacity-50 text-left ">
+			{$userStore.city}
+		</p>
+		<p class=" text-[1.5rem] leading-[2.5rem] text-black opacity-50 text-left ">{$userStore.zip}</p>
+		<p class=" text-[1.5rem] leading-[2.5rem] text-black opacity-50 text-left ">
+			{$userStore.country}
+		</p>
+	</div>
+	<div
+		class="buttons flex flex-col gap-y-[1.5rem] justify-between items-center w-full sm:flex-row ">
+		<Button
+			btnType="grey"
+			content="save for later"
+			on:click={() => dispatch('saveForLater')}
+			link="/" />
+		<Button btnType="primary" content="place order" on:click={() => dispatch('placeOrder')} />
+	</div>
 </div>
